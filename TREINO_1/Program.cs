274c89e811819139aef22ee95275e40a584fcc26
Vitor Hugo <123844614+VH-DM@users.noreply.github.com>
2024-02-28 14:27:24 -0300

    Add files via upload

---
+++ TREINO_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+class Program{
+
+    static void Main(){
+
+        do{
+            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+            Thread.Sleep(1000);
+            
+        }while(Console.KeyAvailable == false);
+
+            Console.WriteLine("adios *o tempo para*");
+
+    }
+}
